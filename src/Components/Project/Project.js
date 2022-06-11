f32bac14fa4776c84d291aa1f21248d858b1db0d
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-06-11 14:40:46 +0600

    projects section completed

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -3,17 +3,22 @@ import { useNavigate } from 'react-router-dom';
 import './Project.css'
 
 const Project = ({ project }) => {
-    const { id, name, heading, description, technology } = project;
+    const { id, img, name, heading, description, technology, website, github } = project;
     const navigate = useNavigate();
-    const handleToSingleProject = event => {
-        event.preventDefault();
-    }
     return (
         <div class="card project-card">
+            <img style={{ width: '100%', height: '15vw', objectFit: 'cover' }} src={img} class="card-img-top" alt="..." />
             <div class="card-body card-details-project">
-                <h5 class="card-title">{name}</h5>
+                <h5 class="card-title pt-5">{name}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">{heading}</h6>
-                <a href="#" onClick={handleToSingleProject} class="btn-project btn">More Details</a>
+                <p><span style={{ color: 'purple', textAlign: 'center', fontSize: '20px' }}>Feature</span> <br />{description}</p>
+                <p><span style={{ color: 'purple', textAlign: 'center', fontSize: '18px', paddingRight: '10px' }}>Technology:</span>{technology}</p>
+            </div>
+            <div className="profile-options project-btn-card">
+                <a className='btn primary-btn' style={{ textDecoration: 'none', listStyle: 'none', color: 'white' }} href={website} target='_blank'>Website</a>
+                <a href={github} target='_blank'>
+                    <button className='btn highlighted-btn'>Github</button>
+                </a>
             </div>
         </div>
     );
